9e5ec39ef295bf833bc6bf6a04c7ae0f821cb2fa
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-11-09 21:48:35 +0200

    React Navigation up and running

--- Source/Frontend/App.js
+++ Source/Frontend/App.js
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+/*import { View, Text } from "react-native";
 
 export default function App() {
   return (
@@ -13,3 +13,31 @@ export default function App() {
     </View>
   );
 }
+*/
+
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
