beb2ab1d86eb24bf9625b1737ee06f0adcbc15c4
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-11-09 21:50:48 +0200

    Header will not be shown

--- Source/Frontend/App.js
+++ Source/Frontend/App.js
@@ -34,7 +34,11 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
