7591228001ad651bd86ddf884670782a4509d4ca
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-11-09 21:58:16 +0200

    HomeScreenComponent saparated alone

--- Source/Frontend/App.js
+++ Source/Frontend/App.js
@@ -1,36 +1,12 @@
-/*import { View, Text } from "react-native";
-
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
-}
-*/
-
 import * as React from "react";
-import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import HomeScreen from "./screens/Home";
 
 const Stack = createNativeStackNavigator();
 
-function App() {
+const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -42,6 +18,6 @@ function App() {
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default App;
