ed50cd15e3965fce0b9c3b10c58df0264c9562ae
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-11-23 17:53:52 +0200

    AAbstracted Styles

--- Source/Frontend/src/screens/Splash.js
+++ Source/Frontend/src/screens/Splash.js
@@ -5,6 +5,7 @@ import {
   SafeAreaView,
   StyleSheet,
   Image,
+  Dimensions,
 } from "react-native";
 import {
   Card,
@@ -20,7 +21,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   tinyLogo: {
-    marginTop: 40,
+    marginTop: 30,
     marginLeft: 20,
     width: 23,
     height: 25.82,
@@ -28,12 +29,41 @@ const styles = StyleSheet.create({
   businessManDrawing: {
     width: 472,
     height: 378,
+    alignSelf: "center",
   },
+  primaryText: {
+    paddingHorizontal: "17%",
+    textAlign: "center",
+    width: "100%",
+    marginTop: 62,
+    fontSize: 34,
+    lineHeight: 44,
+  },
+  secondaryText: {
+    paddingHorizontal: "9.3%",
+    textAlign: "center",
+    width: "100%",
+    marginTop: 5,
+    fontSize: 16,
+    lineHeight: 26,
+  },
+  button: {
+    marginVertical: 40,
+    marginHorizontal: "15.2%",
+    borderRadius: 12,
+  },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 21,
+  },
+  rightArrow: { width: 16.67, height: 10.67 },
 });
 
 const SplashScreen = () => {
   const theme = useTheme();
+  const windowHeight = Dimensions.get("window").height;
 
+  console.log(windowHeight);
   return (
     <SafeAreaView
       style={{ backgroundColor: theme.colors.background, ...styles.container }}
@@ -46,24 +76,41 @@ const SplashScreen = () => {
         <Image
           style={{
             ...styles.businessManDrawing,
-            alignSelf: "center",
-            overflow: "hidden",
           }}
           source={require("../shapes/SplashScreenBusinessMan.png")}
           resizeMode={"center"}
         />
+        <Text style={{ ...styles.primaryText }}>Find a Perfect Job Match</Text>
+
         <Text
           style={{
-            paddingHorizontal: "17%",
-            textAlign: "center",
-            width: "100%",
-            marginTop: 62,
-            fontSize: 34,
-            lineHeight: 44,
+            ...styles.secondaryText,
           }}
         >
-          Find Perfect Job Match
+          Finding your dream job is more easire and faster with JobHub
         </Text>
+        <Button
+          mode="contained-tonal"
+          style={{ ...styles.button }}
+          buttonColor={theme.colors.surface}
+          onPress={() => {
+            console.log("hi");
+          }}
+          contentStyle={{ paddingVertical: 9 }}
+        >
+          <Text
+            style={{
+              color: theme.colors.buttonText,
+              ...styles.buttonText,
+            }}
+          >
+            Let's Get Started
+          </Text>
+          <Image
+            style={{ ...styles.rightArrow }}
+            source={require("../shapes/RightArrowSplashScreen.png")}
+          />
+        </Button>
       </ScrollView>
     </SafeAreaView>
   );
