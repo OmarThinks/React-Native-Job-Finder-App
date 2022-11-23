c23312e92e4081ba9022a850f154822e2bbec658
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-11-23 18:22:21 +0200

    LoginScreen: Container view styling

--- Source/Frontend/src/screens/LogIn.js
+++ Source/Frontend/src/screens/LogIn.js
@@ -1,7 +1,24 @@
-import { Text } from "react-native-paper";
+import { useTheme, Text } from "react-native-paper";
+import { StyleSheet, SafeAreaView, ScrollView } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 const LogInScreen = () => {
-  return <Text>Log In</Text>;
+  const theme = useTheme();
+
+  return (
+    <SafeAreaView
+      style={{ backgroundColor: theme.colors.background, ...styles.container }}
+    >
+      <ScrollView>
+        <Text>Log In</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default LogInScreen;
