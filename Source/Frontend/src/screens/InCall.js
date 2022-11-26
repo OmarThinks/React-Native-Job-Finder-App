ee76f13fa237dbb5018f764f61fc68e2975cb605
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-11-26 14:36:49 +0200

    InCall: Added Image

--- Source/Frontend/src/screens/InCall.js
+++ Source/Frontend/src/screens/InCall.js
@@ -1,5 +1,29 @@
-const InCallScreen = () =>{
-    return (<></>)
-}
+import { Image, SafeAreaView, View } from "react-native";
+import { Text } from "react-native-paper";
+
+const InCallScreen = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ width: "100%", height: "100%" }}>
+        <Image
+          source={require("../shapes/People/01.png")}
+          resizeMethod={"scale"}
+          style={{ overflow: "hidden", width: "100%", height: "100%" }}
+        />
+        <View
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Text>Hi</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default InCallScreen;
