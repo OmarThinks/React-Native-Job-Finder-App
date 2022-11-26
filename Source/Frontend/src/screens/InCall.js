48c149b6b2730688d54f1dff582b1bbe4cd96bfc
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-11-26 15:20:44 +0200

    Completed Styling the page

--- Source/Frontend/src/screens/InCall.js
+++ Source/Frontend/src/screens/InCall.js
@@ -1,5 +1,5 @@
-import { Image, SafeAreaView, View } from "react-native";
-import { Text, useTheme } from "react-native-paper";
+import { Image, SafeAreaView, TouchableOpacity, View } from "react-native";
+import { Button, Text, useTheme } from "react-native-paper";
 
 const InCallScreen = () => {
   const theme = useTheme();
@@ -31,8 +31,31 @@ const InCallScreen = () => {
               paddingHorizontal: 20,
             }}
           >
-            <Text>Hey</Text>
-            <Text>Hey</Text>
+            <TouchableOpacity>
+              <Image
+                style={{ width: 21, height: 10.5 }}
+                source={require("../shapes/Chevron-left.png")}
+                resizeMode={"contain"}
+              />
+            </TouchableOpacity>
+            <View style={{}}>
+              <Image
+                style={{ width: 76, height: 115 }}
+                source={require("../shapes/People/02.png")}
+                resizeMode={"contain"}
+              />
+              <Image
+                style={{
+                  width: 32,
+                  height: 32,
+                  position: "relative",
+                  top: -16,
+                  left: 22,
+                }}
+                source={require("../shapes/Rotation.png")}
+                resizeMode={"contain"}
+              />
+            </View>
           </View>
           <View style={{ flexGrow: 1 }} />
           <View
